0686259d45ddc7f0f0e524ffff378408373c17d5
Rishabh Soni <100075650+RishabhSoni1903@users.noreply.github.com>
2023-12-07 20:11:28 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './Home';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/loch-assessment">
       <div className="App">
         <Routes>
           <Route exact path='/' element={<Home />}></Route>
