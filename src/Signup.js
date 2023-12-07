f2ec2a3ec2c2294171d279cc38bf5db558380acf
Rishabh Soni <100075650+RishabhSoni1903@users.noreply.github.com>
2023-12-07 21:54:45 +0530

    Checking email done

--- src/Signup.js
+++ src/Signup.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+
+    const isValidEmail = (email) => {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email);
+    };
+
+    const handleGetStarted = () => {
+        if (isValidEmail(email)) {
+            window.open('https://app.loch.one/welcome', '_blank');
+        } else {
+            alert("Enter a valid email!")
+        }
+    }
+
+
     return (
         <div className='signup'>
             <div className="header">Sign up for exclusive access.</div>
-            <div className="emailInput"><input type="text" placeholder='Your email address' id='email_input' /></div>
-            <div className="getStarted"><button id='primaryBtn' >Get started</button></div>
+            <div className="emailInput"><input type="text" onChange={(e) => setEmail(e.target.value)} placeholder='Your email address' id='email_input' /></div>
+            <div className="getStarted"><button id='primaryBtn' onClick={() => handleGetStarted()}>Get started</button></div>
             <div className="label"><p id='label'>You'll receive an email with an invite link to join.</p></div>
         </div>
     )
